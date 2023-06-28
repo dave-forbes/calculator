09d9efabb1ac00a9e82f947bea43cdab44a5b554
David Forbes <davidforbesi@hotmail.com>
2023-06-28 09:36:25 +0100

    fix big in operate function

--- script.js
+++ script.js
@@ -19,13 +19,13 @@ let num2;
 let operator;
 
 function operate(num1, num2, operator) {
-  if (operator = '+') {
+  if (operator == '+') {
     return add(num1, num2);
-  } else if (operator = '-') {
+  } else if (operator == '-') {
     return subtract(num1, num2);
-  } else if (operator = '*') {
+  } else if (operator == '*') {
     return multiply(num1, num2);
-  } else if (operator = '/') {
+  } else if (operator == '/') {
     return divide(num1, num2);
   }
 }
